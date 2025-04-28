0f131dd1b19822a8763969c51157a0bdb4a79a56
Dean Feldman <dean.feldman05@gmail.com>
2025-04-28 12:34:31 +0200

    Commit restarted

---
+++ app/src/main/java/com/example/lendahand/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lendahand;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView signupLink = findViewById(R.id.signupLink);
+        signupLink.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            navController.navigate(R.id.SignUp);
+        });
+    }
+}
